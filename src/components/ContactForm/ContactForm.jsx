06d9d54e7aa8ec096593ec096690532a9a4d1cde
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-07-24 02:20:00 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,59 +1,68 @@
-import { Component } from "react"
-import { v4 as uuid } from "uuid"
+import { Component } from 'react';
+import { v4 as uuid } from 'uuid';
 
 const INITAL_STATE = {
-    name: '',
-    phone: '',
-}
+  name: '',
+  phone: '',
+};
 
 class ContactForm extends Component {
-    state = INITAL_STATE
+  state = INITAL_STATE;
 
-    handleChangeForm = ({ target }) => {
-        const {name, value} = target
-        this.setState( { [name]: value})
-    }
+  handleChangeForm = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
 
-handleFormSubmit = (el) => {
-    el.preventDefault()
+  handleFormSubmit = el => {
+    el.preventDefault();
 
     const { name, phone } = this.state;
     const { onAdd } = this.props;
 
-const isValidateForm = this.validateForm()
+    const isValidateForm = this.validateForm();
 
-if(!isValidateForm) return
+    if (!isValidateForm) return;
 
-onAdd ({id: uuid(), name, phone })
-}
+    onAdd({ id: uuid(), name, phone });
+  };
 
-validateForm = () => {
+  validateForm = () => {
     const { name, phone } = this.state;
-    const { onCheckUnique } = this.props
+    const { onCheckUnique } = this.props;
     if (!name || !phone) {
-        alert('Some filed is enpty')
-        return false
+      alert('Some filed is enpty');
+      return false;
     }
 
-    return onCheckUnique(name)
+    return onCheckUnique(name);
+  };
+
+  resetForm = () => this.setState(INITAL_STATE);
+
+  render() {
+    const { name, phone } = this.state;
+    
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={this.handleChangeForm}
+        />
+        <input
+          type="tel"
+          name="phone"
+          placeholder="Number"
+          value={phone}
+          onChange={this.handleChangeForm}
+        />
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
 }
 
-resetForm = () => this.setState(INITAL_STATE)
-
-    render() {
-        const { name, phone } = this.state
-        return(
-            <form onSubmit={this.handleFormSubmit}>
-                <input type="text" name="name" placeholder="Name" value={name} onChange = {this.handleChangeForm} />
-                <input type="tel" name="phone" placeholder="Number" value={phone} onChange = {this.handleChangeForm} />
-                <button type="submit">Add contact</button>
-            </form>
-        )
-    }
-}
-
-
-export default ContactForm 
-
-
-
+export default ContactForm;
